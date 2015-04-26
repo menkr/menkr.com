d23ab40c254a261de01607f11388b5df788d6d8d
Guo Yu <i@guoyu.me>
2015-04-26 16:33:25 +0800

    Made things work

--- menkr.com.js
+++ menkr.com.js
@@ -1,11 +1,11 @@
 var path = require('path')
 var express = require('express');
 var engines = require('consolidate');
-var configs = require('./configs.json');
 
 var app = express();
 
-app.engine('html', engines.swig);
+app.engine('html', engines.swig)
+app.set('view engine', 'html');
 app.set('views', path.resolve(__dirname, './views'));
 app.use(express.static('public'));
 
@@ -13,4 +13,4 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
-app.listen(configs.port || 3000);
+app.listen(process.env.PORT || 3000);
